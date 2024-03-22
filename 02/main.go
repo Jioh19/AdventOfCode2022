e4df0d803d8e0a4fc94148faa0672d8969770ad8
jioh19 <jioh1982@gmail.com>
2024-03-22 12:12:05 -0300

    Day 02 Part 1

--- 02/main.go
+++ 02/main.go
@@ -3,29 +3,57 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 )
 
 type Rounds struct {
-	round [][]byte
+	round [][]string
 }
 
 func main() {
-	fileName := "test.txt"
+	fileName := "input.txt"
 	file, err := os.ReadFile(fileName)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	rounds := insertData(file)
+	m := make(map[string]int)
+	m["AX"] = 3
+	m["AY"] = 6
+	m["BY"] = 3
+	m["BZ"] = 6
+	m["CZ"] = 3
+	m["CX"] = 6
+	m["X"] = 1
+	m["Y"] = 2
+	m["Z"] = 3
 	s1 := time.Now()
+	rounds := insertData(file)
+	result1 := countPoints(rounds, m)
+	fmt.Println(result1)
+	fmt.Println("Time in nanoseconds:", time.Since(s1).Nanoseconds())
 
 }
 
 func insertData(file []byte) Rounds {
-	data := strings.Split(string(file), "\n\r\n")
+	var result Rounds
+	data := strings.Split(string(file), "\n")
 	for _, datum := range data {
+		round := strings.Split(datum, " ")
+		result.round = append(result.round, round)
+	}
+	return result
+}
+
+func countPoints(rounds Rounds, m map[string]int) int {
+	result := 0
+	for _, round := range rounds.round {
+		result += m[round[1]]
+		fmt.Println(m[round[1]], m[round[0]+round[1]])
+		result += m[round[0]+round[1]]
 
 	}
+	return result
 }
