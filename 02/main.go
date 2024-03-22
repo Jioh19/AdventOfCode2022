9c024a66c9ba48f2926118dcb808a0ab6b547aea
jioh19 <jioh1982@gmail.com>
2024-03-22 12:29:53 -0300

    Day 02 Part 2

--- 02/main.go
+++ 02/main.go
@@ -32,8 +32,13 @@ func main() {
 	s1 := time.Now()
 	rounds := insertData(file)
 	result1 := countPoints(rounds, m)
-	fmt.Println(result1)
+	fmt.Println("Part 1: ", result1)
 	fmt.Println("Time in nanoseconds:", time.Since(s1).Nanoseconds())
+
+	s2 := time.Now()
+	result2 := countPoints2(rounds)
+	fmt.Println("Part 2: ", result2)
+	fmt.Println("Time in nanoseconds:", time.Since(s2).Nanoseconds())
 
 }
 
@@ -51,9 +56,30 @@ func countPoints(rounds Rounds, m map[string]int) int {
 	result := 0
 	for _, round := range rounds.round {
 		result += m[round[1]]
-		fmt.Println(m[round[1]], m[round[0]+round[1]])
 		result += m[round[0]+round[1]]
+	}
+	return result
+}
 
+func countPoints2(rounds Rounds) int {
+	result := 0
+
+	m := make(map[string]int)
+	m["AX"] = 3
+	m["AY"] = 1
+	m["AZ"] = 2
+	m["BY"] = 2
+	m["BZ"] = 3
+	m["BX"] = 1
+	m["CZ"] = 1
+	m["CX"] = 2
+	m["CY"] = 3
+	m["X"] = 0
+	m["Y"] = 3
+	m["Z"] = 6
+	for _, round := range rounds.round {
+		result += m[round[1]]
+		result += m[round[0]+round[1]]
 	}
 	return result
 }
